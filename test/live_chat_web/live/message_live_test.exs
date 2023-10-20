fc6401655f45e3607440873183d34ab46c98b41c
Sergio Ocón <sergio.ocon@gmail.com>
2023-10-20 09:00:49 +0200

    deleting obsolete tests

--- test/live_chat_web/live/message_live_test.exs
+++ test/live_chat_web/live/message_live_test.exs
@@ -13,67 +13,17 @@ defmodule LiveChatWeb.MessageLiveTest do
     %{message: message}
   end
 
-  describe "Chat" do
+  describe "Conversation" do
     setup [:create_message]
 
     test "lists all messages", %{conn: conn, message: message} do
-      {:ok, _index_live, html} = live(conn, ~p"/messages")
+      {:ok, _index_live, html} = live(conn, ~p"/")
 
       assert html =~ "Listing Messages"
       assert html =~ message.name
     end
 
-    test "saves new message", %{conn: conn} do
-      {:ok, index_live, _html} = live(conn, ~p"/messages")
-
-      assert index_live |> element("a", "New Message") |> render_click() =~
-               "New Message"
-
-      assert_patch(index_live, ~p"/messages/new")
-
-      assert index_live
-             |> form("#message-form", message: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      assert index_live
-             |> form("#message-form", message: @create_attrs)
-             |> render_submit()
-
-      assert_patch(index_live, ~p"/messages")
-
-      html = render(index_live)
-      assert html =~ "Message created successfully"
-      assert html =~ "some name"
-    end
-
-    test "updates message in listing", %{conn: conn, message: message} do
-      {:ok, index_live, _html} = live(conn, ~p"/messages")
-
-      assert index_live |> element("#messages-#{message.id} a", "Edit") |> render_click() =~
-               "Edit Message"
-
-      assert_patch(index_live, ~p"/messages/#{message}/edit")
-
-      assert index_live
-             |> form("#message-form", message: @invalid_attrs)
-             |> render_change() =~ "can&#39;t be blank"
-
-      assert index_live
-             |> form("#message-form", message: @update_attrs)
-             |> render_submit()
-
-      assert_patch(index_live, ~p"/messages")
-
-      html = render(index_live)
-      assert html =~ "Message updated successfully"
-      assert html =~ "some updated name"
-    end
-
-    test "deletes message in listing", %{conn: conn, message: message} do
-      {:ok, index_live, _html} = live(conn, ~p"/messages")
-
-      assert index_live |> element("#messages-#{message.id} a", "Delete") |> render_click()
-      refute has_element?(index_live, "#messages-#{message.id}")
+    test "new message", %{conn: conn} do
     end
   end
 end
