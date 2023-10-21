b1084dc8a6f1286eb6caac4e0bd2061f6c081f43
Sergio Ocón <sergio.ocon@gmail.com>
2023-10-21 20:14:33 +0200

    some tests

--- test/live_chat_web/live/message_live_test.exs
+++ test/live_chat_web/live/message_live_test.exs
@@ -4,6 +4,7 @@ defmodule LiveChatWeb.MessageLiveTest do
   import Phoenix.LiveViewTest
   import LiveChat.ChatsFixtures
 
+
   @create_attrs %{name: "some name", content: "some content"}
   @update_attrs %{name: "some updated name", content: "some updated content"}
   @invalid_attrs %{name: nil, content: nil}
@@ -13,17 +14,44 @@ defmodule LiveChatWeb.MessageLiveTest do
     %{message: message}
   end
 
+
+  describe "Form" do
+
+
+    test "name can't be blank", %{conn: conn} do
+      {:ok, view, _html} = live(conn, "/")
+
+      assert view
+        |> form("#message-form")
+        |> render_submit(%{name: "", content: "hello"}) =~ "can&#39;t be blank"
+    end
+
+    test "content can't be blank", %{conn: conn} do
+      {:ok, view, _html} = live(conn, "/")
+
+      assert view
+        |> form("#message-form")
+        |> render_submit(%{name: "Name", content: ""}) =~"can&#39;t be blank"
+    end
+
+    test "with both fields properly filled there is no error message", %{conn: conn} do
+      {:ok, view, _html} = live(conn, "/")
+
+      refute view
+        |> form("#message-form", message: %{name: "Name", content: "Content"})
+        |> render_submit() =~ "can&#39;t be blank"
+    end
+  end
+
   describe "Conversation" do
     setup [:create_message]
 
     test "lists all messages", %{conn: conn, message: message} do
       {:ok, _index_live, html} = live(conn, ~p"/")
 
+      assert html =~ "Liveview Chat"
       assert html =~ "Listing Messages"
       assert html =~ message.name
-    end
-
-    test "new message", %{conn: conn} do
     end
   end
 end
